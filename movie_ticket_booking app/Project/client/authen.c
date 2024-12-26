bb841440fd743134131cc53c20dbec1fdef51929
hieunv-ict <nvhieu.ict@gmail.com>
2024-12-27 01:12:09 +0700

    Save changes

--- movie_ticket_booking app/Project/client/authen.c
+++ movie_ticket_booking app/Project/client/authen.c
@@ -46,11 +46,7 @@ int signup(int socketfd){
     printf("\n1. Ticket Buyer\n2. Sale Manager\n3. Admin\nChoose a role: ");
     scanf("%d", &role_choice);
     // copy username
-<<<<<<< HEAD
-    uname = username;
-=======
     // strcpy(uname, username);
->>>>>>> main
     // concatenate all data fields into a message with correct format
     // convert signal of enum type to string
     if (role_choice==1) strcpy(role, "Ticket Buyer");
